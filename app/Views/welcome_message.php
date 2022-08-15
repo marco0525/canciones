483ad6cd31aef3b50352a1e26080652a1d50149e
marco0525 <marco_0525@outlook.com>
2022-08-15 00:17:45 -0500

    Se agrego link para github y whatsapp

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -180,9 +180,8 @@
             <footer class="my-5 pt-5 text-muted text-center text-small">
                 <p class="mb-1">Hecho por Marco Antonio Mendez</p>
                 <ul class="list-inline">
-                    <li class="list-inline-item"><a href="#">Privacy</a></li>
-                    <li class="list-inline-item"><a href="#">Terms</a></li>
-                    <li class="list-inline-item"><a href="#">Support</a></li>
+                    <li class="list-inline-item"><a href="https://github.com/marco0525/canciones">GitHub</a></li>
+                    <li class="list-inline-item"><a href="https://wa.link/z2dqgj">Whatsapp</a></li>
                 </ul>
             </footer>
         </div>
